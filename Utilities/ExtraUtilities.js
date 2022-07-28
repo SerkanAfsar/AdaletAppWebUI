7cb779d3d37f394d5de4e13572ae5a7ad8cca44f
CrimsonSky <serkan-afsar@hotmail.com>
2022-07-28 11:30:11 +0300

    Category DEtail News Show Count Init

--- Utilities/ExtraUtilities.js
+++ Utilities/ExtraUtilities.js
@@ -1,7 +1,6 @@
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 export const convertToLocalDate = (dateTime) => {
-
     return new Date(dateTime).toLocaleDateString('tr-TR', options);
 }
 
